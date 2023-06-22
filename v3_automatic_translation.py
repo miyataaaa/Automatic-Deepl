2830ecc80a87677d3873694759ce2867dbd0731e
Rentarou <miyataaaa1203@gmail.com>
2023-06-22 15:51:25 +0900

    debug

--- v3_automatic_translation.py
+++ v3_automatic_translation.py
@@ -11,7 +11,6 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 import math 
 import re
-from copy import deepcopy
 options = Options()
 options.add_argument('--disable-gpu')
 options.add_argument('--headless')
@@ -21,10 +20,10 @@ options.add_argument('--proxy-server="direct://"')
 options.add_argument('--proxy-bypass-list=*')
 options.add_argument('--start-maximized')
 lock = Lock()
-
+    
 class auto_translator:
     
-    def __init__(self, wating_sec: int=2, **kwargs):
+    def __init__(self, **kwargs):
         self.dirpath = kwargs['Fpath']
         self.fname = kwargs['Fname']
         self.fpath = os.path.join(self.dirpath, self.fname)
@@ -34,12 +33,9 @@ class auto_translator:
         self.eg_sentence = self._sentence_from_docx()
         self.translated_sentence = []
         self.max_worker = kwargs['max_worker']
-        self.replace_dict_jp_to_jp = kwargs['replace_dict_jp_to_jp'] if len(kwargs['replace_dict_jp_to_jp']) != 0 else None
-        self.replace_flag_jp_to_jp = True if type(self.replace_dict_jp_to_jp) == dict else False
-        self.replace_dict_eg_to_jp = kwargs['replace_dict_eg_to_jp'] if len(kwargs['replace_dict_eg_to_jp']) != 0 else None
-        self.replace_flag_eg_to_jp = True if type(self.replace_dict_eg_to_jp) == dict else False
+        self.replace_dict = kwargs['replace_dict'] if len(kwargs['replace_dict']) != 0 else None
+        self.replace_flag = True if type(self.replace_dict) == dict else False
         self.japaneseFont = kwargs['japaneseFont']
-        self.wating_sec = wating_sec
         
         
     def _search_insertPt(self):
@@ -83,30 +79,19 @@ class auto_translator:
             #sentences_jp.append(column_jp.cells[i])
         no_null_sentence = [text for text in sentences_eg if text != ""] # 空文字を消去する
         print(f"Total sensentces in this articl = {len(no_null_sentence)}")
-
         return no_null_sentence
             
     
-    def _keyword_replace(self, sentence, type_of_dict: str="jp_to_jp"):
+    def _keyword_replace(self, sentence):
         
-        """self.replace_dict_jp_to_jpで指定された単語変換を行う。2重変換が起きないようにマッチしたら直接変換するのでなく、
+        """self.replace_dictで指定された単語変換を行う。2重変換が起きないようにマッチしたら直接変換するのでなく、
         辞書内の順番を示すインデックス値に1度変換してからstr.format()メソッドを使用して一斉に変換する。"""
         
         index_list = []
-        replace_dict = self.replace_dict_jp_to_jp if type_of_dict == "jp_to_jp" else self.replace_dict_eg_to_jp
-
-        "{ ←波カッコが文章中に含まれると最後の一括変換時にご認識されるので置換しておく"
-        "具体的には、for文の1行面で、置換したいキーワードをそれが出てきた順番を波カッコで囲う形にしているのでそれに反応する。"
-        sentence = sentence.replace("{", "$")
-        sentence = sentence.replace("}", "!")
-
-        for i, item in enumerate(replace_dict.items()):
+        for i, item in enumerate(self.replace_dict.items()):
             sentence = sentence.replace(item[0], '{'+str(i)+'}')
             index_list.append(item[1])
         sentence = sentence.format(*index_list)
-        sentence = sentence.replace("$", "{")
-        sentence = sentence.replace("!", "}")
-
         return sentence
 
     def _translation_deepl(self, sourse_texts=[]):
@@ -125,22 +110,20 @@ class auto_translator:
         translated_texts = []
         for i in range(len(sourse_texts)):         
             sourse_text = sourse_texts[i]
-
-            # セクション区切りをver4では廃止。単純にすべての英文が翻訳されるように変更(2022/07/01)
-    #         is_numeric = re.match("\d", sourse_text)
-    #         # else句以外はタイトルまたは数式判定のもので一致した場合はcontinueで次のループ要素へジャンプ
-    #         if type(is_numeric) is re.Match:
-    #             print("i={} continue\n: {}\n".format(i, sourse_text))
-    #             translated_texts.append("")
-    # #             print("continue\n")
-    #             continue
-    #         elif len(sourse_text) < 10: # 数式の場合は"Eq.1"など短い文章で示している想定なのでそれらをスキップ
-    #             print("i={} continue\n: {}\n".format(i, sourse_text))
-    #             translated_texts.append("")
-    # #             print("continue\n")
-    #             continue
-    #         else:
-            print("i={}\n: {}\n".format(i, sourse_text))
+            is_numeric = re.match("\d", sourse_text)
+            # else句以外はタイトルまたは数式判定のもので一致した場合はcontinueで次のループ要素へジャンプ
+            if type(is_numeric) is re.Match:
+                print("i={} continue\n: {}\n".format(i, sourse_text))
+                translated_texts.append("")
+    #             print("continue\n")
+                continue
+            elif len(sourse_text) < 10: # 数式の場合は"Eq.1"など短い文章で示している想定なのでそれらをスキップ
+                print("i={} continue\n: {}\n".format(i, sourse_text))
+                translated_texts.append("")
+    #             print("continue\n")
+                continue
+            else:
+                print("i={}\n: {}\n".format(i, sourse_text))
 
             stextarea = browser.find_element_by_css_selector(
                 '.lmt__textarea.lmt__source_textarea.lmt__textarea_base_style')
@@ -163,9 +146,9 @@ class auto_translator:
             lock.acquire()
             # 完全に翻訳されるまで繰り返す。判定は翻訳後文章の文字数が0かどうか
             # 文字数が0の場合は繰り返される。
-            sec = self.wating_sec
             while not translated_text:
                 # print("keep now....")
+                sec = 2
                 time.sleep(sec)
                 translated_text = ttextarea.get_property('value')
                 print("translated_text: \n{}\n len: {}\n ".format(translated_text, len(translated_text))) 
@@ -174,12 +157,12 @@ class auto_translator:
                     print("\ntry again\n")
                     translated_text = ''
                     sec += 2
-                elif len(translated_text) == 0: # 翻訳されていない場合（[...]すら表示されていないパターン）
+                elif len(translated_text) < 5:
                     print("\ntry again\n")
                     translated_text = ''
                     sec += 2
                 else:
-                    if self.replace_flag_jp_to_jp:
+                    if self.replace_flag:
                         translated_texts.append(self._keyword_replace(translated_text))
                     else:
                         translated_texts.append(translated_text)
@@ -213,15 +196,8 @@ class auto_translator:
         """
         _translation_deepl関数を、引数辞書で指定した論理プロセッサ数で処理する。
         """
-
-        # 翻訳前に専門用語（英語）を日本語に変換する（元の英文は変更しないように深いコピーをする）
-        if self.replace_flag_eg_to_jp == True:
-            sentences = deepcopy(self.eg_sentence)
-            sentences = [self._keyword_replace(sentence, type_of_dict="eg_to_jp") for sentence in sentences]
-        else:
-            sentences = self.eg_sentence
-
-        split_list = self._split_sentences(sentences=sentences)
+        
+        split_list = self._split_sentences(sentences=self.eg_sentence)
         print("len(split_list): {}".format(len(split_list)))
         for i in range(len(split_list)):
             texts = split_list[i]
@@ -262,26 +238,31 @@ class auto_translator:
             for run in t_runs:
                 run.font.name = self.japaneseFont
                 run._element.rPr.rFonts.set(qn('w:eastAsia'), run.font.name)
-            
-            # Ver4ではセクション区切りを廃止したため以下はコメントアウト（2022/07/01)
             # セクションを示す文字列なら文字のスタイルをボルドーにする。
             # 例： 1.Introduction -> 文字スタイルをボルドーに変換。
             # セクションかどうかの判定は先頭の文字が数字かどうかで判定している。
-            # is_numeric = re.match("\d", self.eg_sentence[i-self.insert_pt])
-            # if type(is_numeric) is re.Match:
-            #     o_runs = o_cells[i].paragraphs[0].runs
-            #     for run in o_runs:
-            #         if run.text:
-            #             run.bold = True
+            is_numeric = re.match("\d", self.eg_sentence[i-self.insert_pt])
+            if type(is_numeric) is re.Match:
+                o_runs = o_cells[i].paragraphs[0].runs
+                for run in o_runs:
+                    if run.text:
+                        run.bold = True
 
 
         self.docxfile.save(self.fpath)
 
+# def main(**kwargs):
+    
+#     translator = auto_translator(**kwargs)
+#     translator.run_multiThread().saveFile()
+    # sentences = sentence_from_docx(**kwargs)
+    # translated_texts = run_multiThread(sentences=sentences, **kwargs)
+    # saveFile(sentences, translated_texts, **kwargs)
     
 if __name__ == "__main__":
     
     # 翻訳後に専門用語を正しく置換するための辞書オブジェクト
-    word_dict_jp_to_jp = {
+    word_dict = {
                 "ニックスゾーン": "ニックゾーン",
                 "ナイフゾーン": "ニックゾーン",
                 "ニッケルゾーン": "ニックゾーン",
@@ -289,11 +270,7 @@ if __name__ == "__main__":
                 "キックポイント": "ニックポイント",
                 "ナイフポイント": "ニックポイント",
                 "ノッチポイント": "ニックポイント",
-                "ノックポイント": "ニックポイント",
-                "ニッケル点": "ニックポイント",
                 "ニッケルポイント": "ニックポイント",
-                "コツポイント": "ニックポイント",
-                "刻み目": "ニックポイント",
                 "浸食": "侵食",
                 "切り込み": "侵食",
                 "切開": "侵食",
@@ -320,15 +297,11 @@ if __name__ == "__main__":
                 "排水面積": "流域面積",
                 "排水路面積": "流域面積",
                 "寄与面積": "流域面積",
-                "救済": "レリーフ",
-                "浮き上がり": "レリーフ",
                 "盆地": "流域",
                 "ディバイド" : "分水界",
                 "分水嶺" : "分水界",
-                "排水溝": "分水界",
                 "流力モデル":"ストリームパワーモデル",
                 "流線力法則":"ストリームパワーモデル",
-                "流線力則":"ストリームパワーモデル",
                 "流線力侵食モデル":"ストリームパワーモデル",
                 "河道パワーインシジョンモデル":"ストリームパワーモデル",
                 "います。" : "いる。",
@@ -347,53 +320,20 @@ if __name__ == "__main__":
                 "オージュ":"造山帯",
                 "編組河川":"網状河川",
                 "塩水噴出粒":"掃流砂",
-                "塩害":"サルテーション",
-                "塩分":"サルテーション",
-                "塩水化":"サルテーション化",
                 "堤防":"側壁",
                 "点棒":"ポントバー",
                 "ベンド":"湾曲部",
-                "正投影度":"蛇行度",
-                "洞道率":"蛇行度",
-                "洞道力":"蛇行度",
-                "洞道度":"蛇行度",
-                "洞窟度":"蛇行度",
-                "正弦度":"蛇行度",
-                "メアンダー":"蛇行",
-                "洞窟":"蛇行",
                 "現地観測":"フィールド調査",
-                "現地調査":"フィールド調査",
-                "水路図":"ハイドログラフ",
-                "水力半径":"径深",
-                "接水面積":"潤辺",
-                "湿潤周囲":"潤辺",
                 }
     
-    word_dict_eg_to_jp = {
-                "knickpoints" : "ニックポイント",
-                "knickpoint" : "ニックポイント",
-                "base level" : "ベースレベル",
-                "relief" : "レリーフ",
-                "sinuosity":"蛇行度",
-                "knickzone" : "ニックゾーン",
-                "abrasion" : "アブレージョン",
-                "plucking" : "プラッキング",
-                "reach scale": "リーチスケール", 
-                "reach-scale": "リーチスケール", 
-                "stream power": "ストリームパワー",
-                "discharge" : "流量",
-                "saltation" : "サルテーション",
-                }
-    
-    kwargs = {"Fpath": r"F:\研究関連\文献リスト\データ解析手法", # 要約対象のwordファイルが格納されているディレクトリ
-              "Fname": r"Brunton et al 2016.docx", # wordファイル名（拡張子込みで指定する）
-              "max_worker": 5, # 同時実行スレッド数。もし１つのスレッドで例外が発生した場合、デッドロックになってしまいエラーが伝搬してこないので、デバックする際は1スレッドに変更する
-              "replace_dict_jp_to_jp" : word_dict_jp_to_jp,# 翻訳後に専門用語を正しく置換するための辞書(上記の辞書オブジェクト)
-              "replace_dict_eg_to_jp" : word_dict_eg_to_jp, # 翻訳前に専門用語を日本語に変換してからDeeplに渡すことを想定
+    kwargs = {"Fpath": r"E:\研究関連\文献リスト\LEM", # 要約対象のwordファイルが格納されているディレクトリ
+              "Fname": r"sample.docx", # wordファイル名（拡張子込みで指定する）
+              "max_worker": 4, # 同時実行スレッド数。各PC事に最大値が違うので要確認。例外が発生してもデッドロックになってしまい特定場合は1に変更する
+              "replace_dict" : word_dict,# 翻訳後に専門用語を正しく置換するための辞書(上記の辞書オブジェクト)
               "japaneseFont" : "游明朝 (本文のフォント - 日本語)", # 翻訳された日本語のフォントを指定（詳しいフォント名はwordを参照）
              } 
     
-    translator = auto_translator(wating_sec=2, **kwargs)
+    translator = auto_translator(**kwargs)
     translator.run_multiThread()
     translator.saveFile()
     
